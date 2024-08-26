3b5f1a988b7a7b57b7b1cd429b5a84f67b4a9326
Rimkus509 <haitianrimkus@gmail.com>
2024-08-26 13:42:44 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "18494717234"
+global.ownername = "TMP FAMILY"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '18494717234'  //creator number
+global.ownername = 'TMP JAMES' //owner name
+global.botname = 'TMP FAMILY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
